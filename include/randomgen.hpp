687388b9885d4f487aa6b6500a3aac31a1f9143f
maojrs <maojrs@gmail.com>
2018-07-25 16:35:05 +0200

    Forgot to add randomgen

---
+++ include/randomgen.hpp
@@ -0,0 +1,23 @@
+//
+// Created by maojrs on 7/25/18.
+//
+#pragma once
+#include <random>
+//#include <iostream>
+
+#ifndef MSMRD2_RANDOMGEN_HPP
+#define MSMRD2_RANDOMGEN_HPP
+#endif //MSMRD2_RANDOMGEN_HPP
+
+class randomgen {
+private:
+    long seed;
+    std::random_device rd;
+    std::mt19937_64 mt_rand;  // random number generator
+public:
+    randomgen() = default;
+
+    void setSeed(long newseed);
+    double uniformRange(double rmin, double rmax);
+    double normal(double mean, double stddev);
+};
---
+++ src/randomgen.cpp
@@ -0,0 +1,25 @@
+//
+// Created by maojrs on 7/25/18.
+//
+#include "randomgen.hpp"
+
+// Sets seed for random generator mt19937_64
+void randomgen::setSeed(long newseed){
+    if (newseed == -1){
+        mt_rand = std::mt19937_64(rd()); //random device seed
+    } else {
+        mt_rand = std::mt19937_64(newseed); //fixed seed
+    }
+};
+
+// Returns random number between rmin and rmax sampled uniformly
+double randomgen::uniformRange(double rmin, double rmax){
+    std::uniform_real_distribution<double> uniform(rmin,rmax);
+    return uniform(mt_rand);
+};
+
+// Returns random number from normal distribution with given mean and standard deviation
+double randomgen::normal(double mean, double stddev){
+    std::normal_distribution<double> normaldist (mean,stddev);
+    return normaldist(mt_rand);
+};
