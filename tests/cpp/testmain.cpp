37a9a329322074ea9d8e0b88cafa1bc6ce689bcf
maojrs <maojrs@gmail.com>
2018-09-25 16:46:55 +0200

    Addes pair potentials test test and cleaned up example

--- tests/cpp/testmain.cpp
+++ tests/cpp/testmain.cpp
@@ -3,6 +3,8 @@
 //
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
+#include "potentials/gayBerne.hpp"
+#include "potentials/patchyParticle.hpp"
 #include "msm.hpp"
 #include "particle.hpp"
 #include "quaternion.hpp"
@@ -188,4 +190,46 @@ TEST_CASE("Fundamental CTMSM parameters and propagation test", "[ctmsm]") {
         partMS2.updateState();
         REQUIRE(partMS.getState() == partMS2.getState());
     }
+}
+
+TEST_CASE("Pair Potentials consistency", "[potentials]") {
+    std::array<vec3<double>, 4> forctorq1;
+    std::array<vec3<double>, 4> forctorq2;
+
+    // Gay Berne potential consistency check
+    auto potentialGB = gayBerne(3.0,5.0,5.0,1.0);
+    double th = 1.1*M_PI/2.0;
+    vec3<double> pos1 = vec3<double>(0.,0.,0.);
+    vec3<double> pos2 = vec3<double>(1.,0.,0.);
+    vec3<double> th1 = vec3<double>(0., 1., 0);
+    vec3<double> th2 = vec3<double>(std::cos(th), std::sin(th),0.);
+    forctorq1 = potentialGB.forceTorque(pos1, pos2, th1, th2);
+    forctorq2 = potentialGB.forceTorque(pos2, pos1, th2, th1);
+    // Check consistency between forces
+    REQUIRE(forctorq1[0] == forctorq2[2]);
+    REQUIRE(forctorq2[0] == forctorq1[2]);
+    // Check consistency between torques
+    REQUIRE(forctorq1[1] == forctorq2[3]);
+    REQUIRE(forctorq2[1] == forctorq1[3]);
+
+    // Patchy particle potential consistency check
+    double sigma = 1.0;
+    double strength = 100.0;
+    auto patch1 = vec3<double> (1.,0.,0.);
+    std::vector<vec3<double>> patchesCoordinates = {patch1};
+    auto potentialPatchyParticle = patchyParticle(sigma, strength, patchesCoordinates);
+    th = 0.9*M_PI;
+    pos1 = vec3<double>(0.,0.,0.);
+    pos2 = vec3<double>(1.0,-0.3,0.);
+    quaternion<double> q1 = quaternion<double>(1.,0.,0., 0.);
+    quaternion<double> q2 = quaternion<double>(std::cos(th/2.0), 0., 0., std::sin(th/2.0));
+    forctorq1 = potentialPatchyParticle.forceTorque(pos1, pos2, q1, q2);
+    forctorq2 = potentialPatchyParticle.forceTorque(pos2, pos1, q2, q1);
+    // Check consistency between forces
+    REQUIRE(forctorq1[0] == forctorq2[2]);
+    REQUIRE(forctorq2[0] == forctorq1[2]);
+    // Check consistency between torques
+    REQUIRE(forctorq1[1] == forctorq2[3]);
+    REQUIRE(forctorq2[1] == forctorq1[3]);
+
 }
