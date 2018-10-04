d60100686ad011119094314f116204cfd762dda6
maojrs <maojrs@gmail.com>
2018-10-04 17:27:30 +0200

    Commented incomplete binding routines

--- src/binding/bindTrajectory.cpp
+++ src/binding/bindTrajectory.cpp
@@ -1,11 +1,6 @@
 #include "binding.hpp"
 #include "trajectory.hpp"
 
-// Needed to connect lists/arrays of particles in python with cpp integrator methods
-using particle = msmrd::particle;
-using particleMS = msmrd::particleMS;
-PYBIND11_MAKE_OPAQUE(std::vector<particle>);
-PYBIND11_MAKE_OPAQUE(std::vector<particleMS>);
 
 namespace msmrd {
     /*
@@ -13,12 +8,9 @@ namespace msmrd {
      */
     void bindSimulation(py::module &m) {
         py::class_<trajectoryPositionOrientation>(m, "trajectoryPositionOrientation")
-                .def(py::init<size_t>())
-                .def("get_sampler", []);
+                .def(py::init<int &, int &>());
+                //.def("get_sampler", );
 
-        // Created c++ compatible particle list/vector/array of particles in python
-        py::bind_vector<std::vector<particle>>(m, "particleList", py::module_local(false));
-        py::bind_vector<std::vector<particleMS>>(m, "particleMSList", py::module_local(false));
     }
 
 }
