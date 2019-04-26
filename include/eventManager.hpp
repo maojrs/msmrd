377b71aaad45428b1ecc7fa33ea636585ad7aea6
maojrs <maojrs@gmail.com>
2019-04-26 18:24:36 +0200

    Created event manager for MSMRD integration

---
+++ include/eventManager.hpp
@@ -0,0 +1,38 @@
+//
+// Created by maojrs on 4/26/19.
+//
+
+#pragma once
+#include <bits/stdc++.h>
+#include <tuple>
+#include <vector>
+
+
+namespace msmrd {
+    /**
+     * Class to manage events (mainly transitions and/or reactions) in MSM/RD algorithm.
+     */
+    class eventManager {
+    public:
+        /*
+         * @param eventList, list of events/reaction/transisition. Each entry of the list has 4 components, time until
+         * event happens, the state to which it will transision (endstate), an array containing the two indexes (in the
+         * particleList) of the particles involved in the event and a string, "in" or "out" to show it is transitioning
+         * to into a bound state or out of it. The list should be always sorted after being updated by ascending time
+         * (first entry).
+         */
+        std::vector<std::tuple<double, int, std::array<int,2>, std::string>> eventList  = {};
+
+        eventManager() {};
+
+        void addEvent(double waitTime, int endState, int part1Index, int part2Index, std::string inORout);
+
+        void removeEvent(int index);
+
+        void removeEvent(std::tuple<double, int, std::array<int,2>, std::string> event);
+
+        void removeEvents(std::vector<std::tuple<double, int, std::array<int,2>, std::string>> events);
+
+        void sort();
+    };
+}
---
+++ src/eventManager.cpp
@@ -0,0 +1,40 @@
+//
+// Created by maojrs on 4/26/19.
+//
+#include "eventManager.hpp"
+
+namespace msmrd {
+    /**
+     *  Implementation of class to manage events (mainly transitions and/or reactions) in MSM/RD algorithm.
+     */
+
+    // Adds an event to the event list
+    void eventManager::addEvent(double waitTime, int endState, int part1Index, int part2Index, std::string inORout) {
+        std::array<int, 2> partIndexes = {part1Index, part2Index};
+        auto event = std::make_tuple(waitTime, endState, partIndexes, inORout);
+        eventList.push_back(event);
+    }
+
+
+    /*
+     * Routines to remove events, note the different possibilities: one using index in the event list, the second one
+     * using the event itself and the third one using a list of events to be erase at once.
+     */
+    void eventManager::removeEvent(int index){
+        eventList.erase(eventList.begin() + index);
+    }
+
+    void eventManager::removeEvent(std::tuple<double, int, std::array<int,2>, std::string> event){
+        std::remove(eventList.begin(), eventList.end(), event);
+    }
+
+    void eventManager::removeEvents(std::vector<std::tuple<double, int, std::array<int,2>, std::string>> events) {
+        // Not clear yet hot to do this, perhaps: std::remove(eventList.begin(), eventList.end(), events);
+    }
+
+
+    // Sorts event list in ascending order using the value of the first entry in the tuple (time).
+    void eventManager::sort() {
+        std::sort(eventList.begin(), eventList.end());
+    }
+}
