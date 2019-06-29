5745ad524733e658745c39f5526f8de45ad8268a
maojrs <maojrs@gmail.com>
2019-06-29 18:50:20 +0200

    Added test for quaternion fucntions

--- tests/cpp/testTools.cpp
+++ tests/cpp/testTools.cpp
@@ -2,6 +2,7 @@
 // Created by maojrs on 5/29/19.
 //
 #include <catch2/catch.hpp>
+#include <randomgen.hpp>
 #include "particle.hpp"
 #include "tools.hpp"
 #include "vec3.hpp"
@@ -19,4 +20,15 @@ TEST_CASE("Relative distance calculation", "[calculateRelativePosition]") {
     relPosition = msmrdtools::calculateRelativePosition(v1, v2, boundaryActive, boundaryType, boxsize);
     vec3<double> refSolution = {-0.4, -0.4, -0.5};
     REQUIRE( (relPosition -refSolution).norm() < 0.0000001 );
+}
+
+TEST_CASE("Switching between quaternion and axis angle representations", "[quaternion2axisangle, "
+                                                                         "axisangle2quaternion]") {
+    randomgen randg = randomgen();
+    double theta = randg.uniformRange(0, 2*M_PI);
+    vec3<double> axisAngleRep = randg.normal3D(0,5);
+    axisAngleRep = theta*axisAngleRep/axisAngleRep.norm();
+    quaternion<double> quatRep = msmrdtools::axisangle2quaternion(axisAngleRep);
+    vec3<double> recoveredAxisAngleRep = msmrdtools::quaternion2axisangle(quatRep);
+    REQUIRE((axisAngleRep - recoveredAxisAngleRep).norm() < 0.000001);
 }
