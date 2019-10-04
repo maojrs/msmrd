96fb58b68076dc5db8ccb1dd4cdec6432f43d54c
maojrs <maojrs@gmail.com>
2019-10-04 16:41:18 +0200

    Updated dimer/pentamer script

--- scripts/pentamer/generateDimerData.py
+++ scripts/pentamer/generateDimerData.py
@@ -1,7 +1,7 @@
 import msmrd2
 import numpy as np
 from msmrd2.integrators import overdampedLangevin as odLangevin
-from msmrd2.potentials import patchyParticleAngular
+from msmrd2.potentials import patchyParticleAngular2
 import msmrd2.tools.particleTools as particleTools
 import msmrd2.tools.analysis as analysisTools
 import multiprocessing
@@ -19,28 +19,27 @@ numAngularMinima = 1
 D = 1
 Drot = 1
 separationDistance = 1.25 # minimum separation distance for initial condition
-numSimulations = 600 #500 #1000 #20 #200
+numSimulations = 6 #500 #1000 #20 #200
 
 
 # Simulation parameters
 timesteps = 3000000 #2000000 #5000000 #400000 #20000000
 bufferSize = 1024
 stride = 25
-outTxt = True
-outH5 = False
-outChunked = False
+outTxt = False
+outH5 = True
+outChunked = True
 trajtype = "patchyDimer"
 
 # Define patchy Particle potential with angular dependence
 sigma = 1.0
-strength = 100.0 #50.0
-angularStrength = 10.0 #10.0
+strength = 160.0 #50.0
+angularStrength = 20.0 #10.0
 angleDiff = 3*np.pi/5.0
 patch1 = np.array([np.cos(angleDiff/2),np.sin(angleDiff/2),0.])
 patch2 = np.array([np.cos(-angleDiff/2),np.sin(-angleDiff/2),0.])
 patchesCoordinates = [patch1, patch2]
-potentialPatchyParticleAngular = patchyParticleAngular(sigma, strength, angularStrength, patchesCoordinates)
-potentialPatchyParticleAngular.setNumAngularMinima(numAngularMinima)
+potentialPatchyParticleAngular2 = patchyParticleAngular2(sigma, strength, angularStrength, patchesCoordinates)
 
 # Define simulaion boundaries (choose either spherical or box)
 boxsize = 5
@@ -52,7 +51,7 @@ parentDirectory = "../../data/pentamer/"
 # parentDirectory = "/group/ag_cmb/scratch/maojrs/msmrd2_data/dimer/"
 
 # Create folder for data
-foldername = "dimertest"
+foldername = "dimer"
 filedirectory =  os.path.join(parentDirectory, foldername)
 try:
     os.mkdir(filedirectory)
@@ -87,7 +86,7 @@ def runParallelSims(simnumber):
     seed = int(-1*simnumber) # random seed (negative and different for every simulation, good for parallelization)
     integrator = odLangevin(dt, seed, bodytype)
     integrator.setBoundary(boxBoundary)
-    integrator.setPairPotential(potentialPatchyParticleAngular)
+    integrator.setPairPotential(potentialPatchyParticleAngular2)
 
     # Creates simulation
     sim = msmrd2.simulation(integrator)
