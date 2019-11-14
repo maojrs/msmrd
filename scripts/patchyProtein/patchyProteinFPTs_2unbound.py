f3f7398d5280631606d987dacc3436e172b2f7cd
maojrs <maojrs@gmail.com>
2019-11-14 19:06:35 +0100

    Added 2nd script for FPT of patchyProetin

--- scripts/patchyProtein/patchyProteinFPTs_2unbound.py
+++ scripts/patchyProtein/patchyProteinFPTs_2unbound.py
@@ -1,17 +1,17 @@
 import numpy as np
 import msmrd2
+import random
 from msmrd2.potentials import patchyProteinMarkovSwitch
 from msmrd2.markovModels import continuousTimeMarkovStateModel as ctmsm
 from msmrd2.integrators import overdampedLangevinMarkovSwitch as odLangevinMS
-import msmrd2.tools.particleTools as particleTools
+import msmrd2.tools.quaternions as quaternions
 import multiprocessing
 from multiprocessing import Pool
 import os
 
 '''
-Creates an MD simulation of two patchyProtein particle and calculates their first passage times (FPTs) from a random 
-unbound configuration to any given bound state(s). The data is written to 
-'../data/patchyProtein/first_passage_times/filename_here.
+Creates an MD simulation of two particle and calculates their first passage times (FPTs) from a given 
+bound state to any unbound state. The data is written to '../data/patchyProtein/first_passage_times/filename_here.
 '''
 
 # Main parameters for particle and integrator
@@ -21,6 +21,8 @@ bodytype = 'rigidbody'
 particleTypes = [0, 1]
 minimumUnboundRadius = 2.5
 numTrajectories = 20 #10000
+numBoundStates = 6
+initialState = 1
 
 # Define Patchy Protein potential parameters (This values are fixed and should match
 # those used to determine metastable states in potential and trajectory.)
@@ -50,8 +52,64 @@ except OSError as error:
     print("First passage times directory already exists. Simulation continues.")
 
 # Create empty files to save the data in parallel algorithm
-filename = parentDirectory  + 'patchyProteinFPTs_2bound_trajs' + str(numTrajectories) \
-           +'_boxsize' + str(boxsize) + '.xyz'
+filename = parentDirectory + 'patchyProteinFPTs_' + initialState + '_2unbound_trajs' \
+           + str(numTrajectories) +'_boxsize' + str(boxsize) + '.xyz'
+
+
+def generateParticleList(state, boxsize, D, Drot, randomSeed = -1):
+    '''
+    Generate a random particle list of two particles corresponding to either state A or state B. As each
+    state has several different configurations, this functions picks one randomly. The definition of the
+    states is taken form patchyProtein.cpp (trajectory)
+    :param state: bound state in which the two particle list should be in, A or B
+    :param D and Drot: diffusion coefficients of particles.
+    :param randomSeed: seed for python random generator. Important to specify in parallel runs. Default value of -1
+    will use the default seed.
+    :return: random particle list corresponding to either state A or state B.
+    '''
+
+    # Transform boxsize to vector if neccesarry.
+    boxsize = boxsize - 1 # to ensure the whole compund is initially inside box (1 is the norm of relpos below)
+    if np.isscalar(boxsize):
+        boxsize = np.array([boxsize, boxsize, boxsize])
+
+    position1 = np.array([0.0, 0.0, 0.0])
+    #position1 = np.array([boxsize[0]*random.random()-0.5*boxsize[0],
+    #                      boxsize[1]*random.random()-0.5*boxsize[1],
+    #                      boxsize[2]*random.random()-0.5*boxsize[2]])
+    orientation1 = np.array([1.0, 0.0, 0.0, 0.0])
+    # Define relative positions
+    relPos = [None]*numBoundStates
+    relPos[0] = np.array([1., 0., 0.])
+    relPos[1] = np.array([0., 1., 0.])
+    relPos[2] = np.array([0., 0., 1.])
+    relPos[3] = np.array([-1., 0., 0.])
+    relPos[4] = np.array([0., -1., 0.])
+    relPos[5] = np.array([0., 0., -1.])
+    # Define relative rotations
+    rotations = [None]*numBoundStates
+    rotations[0] = np.array([0.0, 0.0, np.pi])
+    rotations[1] = np.array([0.0, 0.0, -np.pi / 2.0])
+    rotations[2] = np.array([0.0, np.pi / 2.0, 0.0])
+    rotations[3] = np.array([0.0, 0.0, 0.0])
+    rotations[4] = np.array([0.0, 0.0, np.pi / 2.0])
+    rotations[5] = np.array([0.0, -np.pi / 2.0, 0.0])
+    # Convert axis-angle rotations to quaternions
+    quatRotations = [None]*numBoundStates
+    for i in range(numBoundStates):
+        quatRotations[i] = quaternions.angle2quat(rotations[i])
+
+    # Assign position and orientation depending on initial state A or B
+    substate = initialState # Could be random among all of them, e.g. random.choice([1,2,3,4,5,6])
+    position2 = relPos[substate - 1]
+    orientation2 = quatRotations[substate - 1]
+    part1 = msmrd2.particle(D, Drot, position1, orientation1)
+    part2 = msmrd2.particle(D, Drot, position2, orientation2)
+    partlist = msmrd2.integrators.particleList([part1, part2])
+    return partlist
+
+
+
 
 def simulationFPT(trajectorynum):
     '''
@@ -96,22 +154,21 @@ def simulationFPT(trajectorynum):
     integrator.setBoundary(boxBoundary)
     integrator.setPairPotential(potentialPatchyProtein)
 
-    # Creates random particle list
+    # Generate random position and orientation particle list with two particles
     seed = int(trajectorynum)
-    partlist = particleTools.randomParticleMSList(numparticles, boxsize, minimumUnboundRadius ,
-                                                  particleTypes, unboundMSMlist, seed)
+    partlist = generateParticleList(initialState, boxsize, D, Drot, seed)
 
     # Calculates the first passage times for a given bound state. Each trajectory is integrated until
     # a bound state is reached. The output in the files is the elapsed time.
-    unbound = True
-    while(unbound):
+    bound = True
+    while(bound):
         integrator.integrate(partlist)
         boundState = dummyTraj.getState(partlist[0], partlist[1])
-        if boundState in boundStates:
-            unbound = False
-            return boundState, integrator.clock
-        elif integrator.clock >= 15000.0:
-            unbound = False
+        if boundState == 0:
+            bound = False
+            return initialState, integrator.clock
+        elif integrator.clock >= 10000.0:
+            bound = False
             return 'Failed at:', integrator.clock
 
 
@@ -126,8 +183,8 @@ def multiprocessingHandler():
     with open(filename, 'w') as file:
         for index, result in enumerate(pool.imap(simulationFPT, trajNumList)):
             state, time = result
-            if state in boundStates:
-                file.write(str(state) + ' ' + str(time) + '\n')
+            if state == 'A' or state == 'B':
+                file.write(state + ' ' + str(time) + '\n')
                 print("Simulation " + str(index) + ", done. Success!")
             else:
                 print("Simulation " + str(index) + ", done. Failed :(")
@@ -136,15 +193,6 @@ def multiprocessingHandler():
 # Run parallel code
 multiprocessingHandler()
 
-# Serial code for testing with gdb
-# trajNumList = list(range(numTrajectories))
-# with open(filename, 'w') as file:
-#     for index in range(numTrajectories):
-#         state, time = simulationFPT(index)
-#         if state in boundStates:
-#             file.write(str(state) + ' ' + str(time) + '\n')
-#             print("Simulation " + str(index) + ", done. Success!")
-#         else:
-#             print("Simulation " + str(index) + ", done. Failed :(")
+
 
 
