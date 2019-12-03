83ac80d996044f53ba5d73e956a8d85e0d5b895d
maojrs <maojrs@gmail.com>
2019-12-03 22:52:34 +0100

    Updated bindPotentials

--- src/binding/bindPotentials.cpp
+++ src/binding/bindPotentials.cpp
@@ -62,9 +62,6 @@ namespace msmrd {
         py::class_<patchyProteinMarkovSwitch, patchyProtein>(m, "patchyProteinMarkovSwitch",
                                                  "Patchy protein with Markov Switch potential (sigma, "
                                                  "strength, patches coordinates A, patches coordinates B)")
-                .def(py::init<double &, double &,
-                        std::vector<std::vector<double>> &,
-                        std::vector<std::vector<double>> &>())
                 .def(py::init<double &, double &, double &,
                         std::vector<std::vector<double>> &,
                         std::vector<std::vector<double>> &>());
